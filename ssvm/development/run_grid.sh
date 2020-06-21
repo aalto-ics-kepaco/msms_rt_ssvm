f34f76e7378371a0375cba2d6910d6dd1d67531d
Eric Bach <eric.bach@aalto.fi>
2020-06-21 18:43:20 +0300

    add tensorboard logs

--- ssvm/development/run_grid.sh
+++ ssvm/development/run_grid.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-#SBATCH --cpus-per-task=8 --mem-per-cpu=5000
-#SBATCH --time=12:00:00
-#SBATCH --array=0-71
+#SBATCH --cpus-per-task=18 --mem-per-cpu=3000
+#SBATCH --time=56:00:00
+# -- SBATCH --time=01:00:00 --partition=debug
+# -- SBATCH --array=0-59
+#SBATCH --array=32,34,35
 
-module load annaconda
+module load anaconda
 
 PROJECTDIR="/scratch/cs/kepaco/bache1/projects/rt_msms_ssvm/"
 DATADIR="$PROJECTDIR/_ISMB2016_DATA/"
@@ -14,4 +16,5 @@ SCRIPTPATH="$PROJECTDIR/src/ssvm/development/ssvm_metident__parameter_grid.py"
 srun python $SCRIPTPATH --param_tuple_index="$SLURM_ARRAY_TASK_ID" \
   --input_data_dir="$DATADIR" \
   --output_dir="$LOGDIR" \
-  --n_samples=800
+  --n_samples=1000 \
+  --n_epoch=1000
