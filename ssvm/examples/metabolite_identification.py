6ab735c12e11a0473f89e3688afaaeb0d6bad0d5
--help <eric.bach@aalto.fi>
2020-07-01 19:24:15 +0300

    add performance graph for 250eef3

--- ssvm/examples/metabolite_identification.py
+++ ssvm/examples/metabolite_identification.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
 
     start = timer()
 
-    svm = StructuredSVMMetIdent(C=128, rs=928, n_epochs=3, batch_size=8, stepsize="linesearch") \
+    svm = StructuredSVMMetIdent(C=128, rs=928, n_epochs=10, batch_size=8, stepsize="linesearch") \
         .fit(X_train, mols_train, candidates=cand, num_init_active_vars_per_seq=1,
              train_summary_writer=train_summary_writer)
 
