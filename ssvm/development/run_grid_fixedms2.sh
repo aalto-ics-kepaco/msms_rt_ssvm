283ccb120972408074684337625adb15d89fe95b
Eric Bach <eric.bach@aalto.fi>
2021-01-14 13:32:55 +0200

    small changes

--- ssvm/development/run_grid_fixedms2.sh
+++ ssvm/development/run_grid_fixedms2.sh
@@ -2,17 +2,19 @@
 
 # We should reserve at least 12GB of RAM for the candidate database
 
-#SBATCH --cpus-per-task=16 --mem-per-cpu=2000
-#SBATCH --time=48:00:00
+#SBATCH --cpus-per-task=32 --mem-per-cpu=2000
+#SBATCH --time=04:00:00
 # -- SBATCH --time=01:00:00 --partition=debug
 # -- SBATCH --array=0-44
-#SBATCH --array=1-4
+#SBATCH --array=4
 
+N_THREADS=4
+N_JOBS=8
 
 PROJECTDIR="/scratch/cs/kepaco/bache1/projects/rt_msms_ssvm/"
 DB_DIR="$PROJECTDIR/_CASMI_DB/"
 DB_FN="DB_LATEST.db"
-LOGDIR="$PROJECTDIR/src/ssvm/development/logs_triton/fixedms2_multicore"
+LOGDIR="$PROJECTDIR/src/ssvm/development/logs_triton/fixedms2_multicore_scipy"
 SCRIPTPATH="$PROJECTDIR/src/ssvm/development/ssvm_fixedms2__parameter_grid.py"
 
 # Load the conda environment
@@ -30,13 +32,13 @@ trap "rm -rf $LOCAL_DB_DIR; exit" TERM EXIT
 # Copy the DB file to the node's local disk
 cp "$DB_DIR/$DB_FN" "$LOCAL_DB_DIR"
 
-NUMBA_NUM_THREADS=4 \
+NUMBA_NUM_THREADS=$N_THREADS;OMP_NUM_THREADS=$N_THREADS;OPENBLAS_NUM_THREADS=$N_THREADS; \
     srun python $SCRIPTPATH \
-  --n_jobs=4 \
+  --n_jobs="$N_JOBS" \
   --param_tuple_index="$SLURM_ARRAY_TASK_ID" \
   --db_fn="$LOCAL_DB_DIR/$DB_FN" \
   --output_dir="$LOGDIR" \
   --n_samples_train=250 \
   --n_samples_test=250 \
   --n_epoch=5 \
-  --mol_kernel="minmax_numba"
+  --mol_kernel="minmax"
