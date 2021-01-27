6adc00edd3855bd1926b574acf7496b524f44c3f
Eric Bach <eric.bach@aalto.fi>
2021-01-27 21:22:43 +0200

    remove rtw from grid-search, add baseline topk performance

--- ssvm/experiments/bioinf_redo/fixedms2__gridsearch.py
+++ ssvm/experiments/bioinf_redo/fixedms2__gridsearch.py
@@ -14,12 +14,13 @@ from typing import Tuple
 from matchms.Spectrum import Spectrum
 from sklearn.model_selection import GroupShuffleSplit
 
-from ssvm.data_structures import RandomSubsetCandidateSQLiteDB, FixedSubsetCandidateSQLiteDB, CandidateSQLiteDB
+from ssvm.data_structures import RandomSubsetCandidateSQLiteDB, FixedSubsetCandidateSQLiteDB
 from ssvm.data_structures import LabeledSequence, SequenceSample
 from ssvm.ssvm import StructuredSVMSequencesFixedMS2
 
 from msmsrt_scorer.lib.data_utils import load_dataset_CASMI, load_dataset_EA
 from msmsrt_scorer.experiments.plot_and_table_utils import _marg_or_cand
+from msmsrt_scorer.lib.evaluation_tools import get_topk_performance_from_scores
 
 # ================
 # Setup the Logger
@@ -74,12 +75,12 @@ def get_hparam_estimation_setting(debug: bool) -> Tuple:
         n_splits_inner = 2
         n_epochs_inner = 1
         C_grid = [1, 64]
-        rtw_grid = [0.5]
+        rtw_grid = [None]
     else:
         n_splits_inner = 3
-        n_epochs_inner = 3
-        C_grid = [1, 4, 16, 64, 256]
-        rtw_grid = [0.1, 0.25, 0.5]
+        n_epochs_inner = 4
+        C_grid = [1, 4, 16, 32, 64, 128, 256]
+        rtw_grid = [None]
 
     return n_splits_inner, n_epochs_inner, C_grid, rtw_grid
 
@@ -167,8 +168,7 @@ if __name__ == "__main__":
     spec_ids_eval = [challenges[i]["name"] for i in eval_set]
     spectra_eval, labels_eval = zip(*[(spectrum, label) for spectrum, label in zip(spectra, labels)
                                       if spectrum.get("spectrum_id") in spec_ids_eval])
-
-    labelspace_subset = {challenges[i]["name"]: candidates[i]["structure"] for i in eval_set}
+    labelspace_eval = {challenges[i]["name"]: candidates[i]["structure"] for i in eval_set}
 
     db.close()
 
@@ -188,7 +188,7 @@ if __name__ == "__main__":
     LOGGER.info("C-grid: {}".format(C_grid))
     LOGGER.info("RTW-grid: {}".format(rtw_grid))
     for idx, (C, rtw) in enumerate(param_grid):
-        LOGGER.info("(%d / %d) C = %f, rtw = %f" % (idx + 1, len(param_grid), C, rtw))
+        LOGGER.info("({} / {}) C = {}, rtw = {}".format(idx + 1, len(param_grid), C, rtw))
 
         for jdx, (train, test) in enumerate(cv_inner.split(spectra_train, groups=labels_train)):
             LOGGER.info("(%d / %d, inner) Number of spectra (inner cv): train = %d, test = %d"
@@ -205,7 +205,7 @@ if __name__ == "__main__":
                     db_fn=args.db_fn, molecule_identifier=args.molecule_identifier, random_state=jdx,
                     number_of_candidates=args.max_n_train_candidates, include_correct_candidate=True,
                     init_with_open_db_conn=False),
-                N=np.int(np.round(args.n_samples_train * 0.5)), L_min=10, L_max=30, random_state=jdx,
+                N=np.int(np.round(args.n_samples_train * 0.66)), L_min=10, L_max=30, random_state=jdx,
                 ms2scorer=args.ms2scorer)
 
             # Train the SSVM
@@ -214,7 +214,7 @@ if __name__ == "__main__":
                 mol_feat_label_loss=mol_feat_label_loss, mol_feat_retention_order="substructure_count",
                 mol_kernel=args.mol_kernel, C=C, step_size_approach=args.stepsize, batch_size=args.batch_size,
                 n_epochs=n_epochs_inner, label_loss=label_loss, random_state=jdx,
-                retention_order_weight=rtw, n_jobs=args.n_jobs, n_trees_per_sequence=args.n_trees_for_training
+                retention_order_weight=None, n_jobs=args.n_jobs, n_trees_per_sequence=args.n_trees_for_training
             ).fit(training_sequences, n_init_per_example=args.n_init_per_example)
 
             # Access test set performance
@@ -228,20 +228,18 @@ if __name__ == "__main__":
                     db_fn=args.db_fn, molecule_identifier=args.molecule_identifier, random_state=jdx,
                     number_of_candidates=args.max_n_train_candidates, include_correct_candidate=True,
                     init_with_open_db_conn=False),
-                N=np.int(np.round(args.n_samples_train * 0.5)), L_min=10, L_max=30, random_state=jdx,
+                N=np.int(np.round(args.n_samples_train * 0.33)), L_min=10, L_max=30, random_state=jdx,
                 ms2scorer=args.ms2scorer)
 
             LOGGER.info("Score hyper-parameter tuple ...")
-            opt_values.append([C, rtw, jdx, ssvm.score(test_sequences, stype="ndcg_ohc")])
+            opt_values.append([C, jdx, ssvm.score(test_sequences, stype="ndcg_ohc")])
 
-    df_opt_values = pd.DataFrame(opt_values, columns=["C", "retention_order_weight", "inner_split", "ndcg_ohc"])
-    C_opt, rtw_opt = df_opt_values.groupby(["C", "retention_order_weight"]).mean()["ndcg_ohc"].idxmax()
+    df_opt_values = pd.DataFrame(opt_values, columns=["C", "inner_split", "ndcg_ohc"])
+    C_opt = df_opt_values.groupby(["C"]).mean()["ndcg_ohc"].idxmax()
 
-    LOGGER.info("C_opt=%f, rtw_opt=%f" % (C_opt, rtw_opt))
+    LOGGER.info("C_opt=%f" % C_opt)
     LOGGER.info("C-grid: {}".format(C_grid))
     LOGGER.info(df_opt_values.groupby(["C"]).mean()["ndcg_ohc"].values)
-    LOGGER.info("RTW-grid: {}".format(rtw_grid))
-    LOGGER.info(df_opt_values.groupby(["retention_order_weight"]).mean()["ndcg_ohc"].values)
 
     # =========================================
     # Train the SSVM with best hyper-parameters
@@ -260,7 +258,7 @@ if __name__ == "__main__":
         mol_feat_label_loss=mol_feat_label_loss, mol_feat_retention_order="substructure_count",
         mol_kernel=args.mol_kernel, C=C_opt, step_size_approach=args.stepsize, batch_size=args.batch_size,
         n_epochs=args.n_epochs, label_loss=label_loss, random_state=1993,
-        retention_order_weight=rtw_opt, n_jobs=args.n_jobs, n_trees_per_sequence=args.n_trees_for_training
+        retention_order_weight=None, n_jobs=args.n_jobs, n_trees_per_sequence=args.n_trees_for_training
     ).fit(training_sequences, n_init_per_example=args.n_init_per_example)
 
     # ====================
@@ -268,7 +266,7 @@ if __name__ == "__main__":
     # ====================
     LOGGER.info("=== Evaluate SSVM performance on the evaluation set ===")
     candidates = FixedSubsetCandidateSQLiteDB(
-        labelspace_subset=labelspace_subset, db_fn=args.db_fn, molecule_identifier=args.molecule_identifier,
+        labelspace_subset=labelspace_eval, db_fn=args.db_fn, molecule_identifier=args.molecule_identifier,
         init_with_open_db_conn=False, assert_correct_candidate=True)
 
     # candidates = CandidateSQLiteDB(
@@ -293,6 +291,34 @@ if __name__ == "__main__":
     LOGGER.info("Top-1 (MAP) = %.2f%%" % scores["top1_map"])
     LOGGER.info("Top-1 (MM) = %.2f%%, Top-5 (MM) = %.2f%%, Top-10 (MM) = %.2f%%, Top-20 (MM) = %.2f%%"
                 % (scores["topk_mm"][0], scores["topk_mm"][4], scores["topk_mm"][9], scores["topk_mm"][19]))
+
+    # Performance with original MS2
+    candidates_with_ms2scores = {}
+    with candidates:
+        for s, (spec, lab) in enumerate(zip(spectra_eval, labels_eval)):
+            candidates_with_ms2scores[s] = {"index_of_correct_structure": candidates.get_labelspace(spec).index(lab),
+                                            "score": np.array(candidates.get_ms2_scores(spec, args.ms2scorer)),
+                                            "n_cand": candidates.get_n_cand(spec)}
+            scores["topk_baseline"] = get_topk_performance_from_scores(candidates_with_ms2scores, method="casmi2016")[1]
+        LOGGER.info("BASELINE: Top-1 = %.2f%%, Top-5 = %.2f%%, Top-10 = %.2f%%, Top-20 = %.2f%%"
+                    % (scores["topk_baseline"][0], scores["topk_baseline"][4], scores["topk_baseline"][9],
+                       scores["topk_baseline"][19]))
+
+    # Performance first candidate
+    for s, (spec, lab) in enumerate(zip(spectra_eval, labels_eval)):
+        candidates_with_ms2scores[s]["score"] = np.arange(candidates_with_ms2scores[s]["n_cand"], 0, -1)
+        scores["topk_first_candidate"] = get_topk_performance_from_scores(candidates_with_ms2scores, method="casmi2016")[1]
+    LOGGER.info("FIRST CANDIDATE: Top-1 = %.2f%%, Top-5 = %.2f%%, Top-10 = %.2f%%, Top-20 = %.2f%%"
+                % (scores["topk_first_candidate"][0], scores["topk_first_candidate"][4],
+                   scores["topk_first_candidate"][9], scores["topk_first_candidate"][19]))
+
+    # Performance first candidate
+    for s, (spec, lab) in enumerate(zip(spectra_eval, labels_eval)):
+        candidates_with_ms2scores[s]["score"] = np.random.rand(candidates_with_ms2scores[s]["n_cand"])
+        scores["topk_random"] = get_topk_performance_from_scores(candidates_with_ms2scores, method="casmi2016")[1]
+    LOGGER.info("RANDOM SCORES: Top-1 = %.2f%%, Top-5 = %.2f%%, Top-10 = %.2f%%, Top-20 = %.2f%%"
+                % (scores["topk_random"][0], scores["topk_random"][4],
+                   scores["topk_random"][9], scores["topk_random"][19]))
 
     # =================
     # Write out results
@@ -320,6 +346,5 @@ if __name__ == "__main__":
         ofile.write("C_grid = {}\n".format(C_grid))
         ofile.write("C_opt = %f\n" % C_opt)
         ofile.write("rtw_grid = {}\n".format(rtw_grid))
-        ofile.write("rtw_opt = %f\n" % rtw_opt)
 
     sys.exit(0)
